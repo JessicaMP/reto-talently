f44cdb48425725e0a50a92d7ad82f580c99080a6
MAC <mac@MacBook-Air-de-MAC.local>
2021-08-10 00:14:26 -0500

    34fbcb3

---
+++ _nuxt/7507fd4.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{259:function(e,n,t){"use strict";t.r(n);var r=t(12),component=Object(r.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("charater")])}),[],!1,null,null,null);n.default=component.exports}}]);
